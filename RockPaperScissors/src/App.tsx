ecb7a1905772d92e0c9d0e3ef20d94e9841067dd
Niyomugabo-fabrice <nfabrice058@gmail.com>
2026-02-24 22:28:57 +0200

    adding the header component

--- RockPaperScissors/src/App.tsx
+++ RockPaperScissors/src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Headers from "./Components/Headers"
 
 function App() {
   const [choose, setChoose] = useState("")
-  
   return (
     <div className="main">
+      <Headers/>
+      <h1>You Chooseed {choose} </h1>
       <button onClick={()=>setChoose("Rocky")}>Rock</button>
       <button onClick={()=>setChoose("Paper")}>Paper</button>
       <button onClick={()=>setChoose("Scissors")}>Scissors</button>
-       <h1>You Chooseed {choose} </h1>
+      <div className="bought"></div>
     </div>
   )
 }
---
+++ RockPaperScissors/src/Components/Headers.tsx
@@ -0,0 +1,11 @@
+export default function Headers()
+{
+    return(
+        <div className="heading">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Products</a>
+        <a href="">Contacts</a>
+        </div>
+    )
+}
