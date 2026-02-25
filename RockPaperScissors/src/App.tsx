03d1a2163fad63a07883228b44bd9f2d40bce0fd
Niyomugabo-fabrice <nfabrice058@gmail.com>
2026-02-25 06:53:13 +0200

    orgnizing the buttons

--- RockPaperScissors/src/App.tsx
+++ RockPaperScissors/src/App.tsx
@@ -3,14 +3,15 @@ import './App.css'
 import Headers from "./Components/Headers"
 
 function App() {
-  const [choose, setChoose] = useState("")
+  const [choose, setChoose] = useState("Choose One")
   return (
     <div className="main">
       <Headers/>
-      <h1>You Chooseed {choose} </h1>
-      <button onClick={()=>setChoose("Rocky")}>Rock</button>
-      <button onClick={()=>setChoose("Paper")}>Paper</button>
-      <button onClick={()=>setChoose("Scissors")}>Scissors</button>
+      <h1> {choose} </h1>
+      <button onClick={()=>setChoose("You choosed Rock")}>Rock</button>
+      <button onClick={()=>setChoose("You choosed Paper")}>Paper</button>
+      <button onClick={()=>setChoose("You choosed Scissors")}>Scissors</button>
+      <button onClick={(()=>setChoose("Choose One"))}>Reset</button>
     </div>
   )
 }
